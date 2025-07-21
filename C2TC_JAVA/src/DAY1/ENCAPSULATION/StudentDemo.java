d78a5836a8244ba385b7cbca4321cb04992ab2d6
moggana1808 <moggana18@gmail.com>
2025-07-21 15:46:53 +0530

    addition

---
+++ C2TC_JAVA/src/DAY1/ENCAPSULATION/StudentDemo.java
@@ -0,0 +1,13 @@
+package DAY1.ENCAPSULATION;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		Student ob= new Student();
+		ob.setId(18);
+		ob.setName("moggana");
+		System.out.println(ob.getId());
+		System.out.println(ob.getName());
+	}
+}
+		
