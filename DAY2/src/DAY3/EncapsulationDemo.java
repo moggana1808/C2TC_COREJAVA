280f18171134f626e40e91b49cc4ed1efae6b634
moggana1808 <moggana18@gmail.com>
2025-07-24 09:43:54 +0530

    DAY3

---
+++ DAY2/src/DAY3/EncapsulationDemo.java
@@ -0,0 +1,22 @@
+package DAY3;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Program to demonstrate class and object 
+		
+				
+				//object creation 
+				OopsConceptDemo obj = new OopsConceptDemo();
+				obj.setSerialNum(101);
+				obj.setName("Pooja");
+				obj.setAge(20);
+				System.out.println(obj);
+				
+			}
+
+		
+	}
+
+
